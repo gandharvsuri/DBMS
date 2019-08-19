a0b71838e9cc634637f84f14b7a912ac364a2749
gandharv <gandharv.suri@iiitb.org>
2019-08-19 20:08:41 +0530

    Added Error Code

--- Lab Session 2b-20190816/pds.c
+++ Lab Session 2b-20190816/pds.c
@@ -14,6 +14,7 @@ int pds_create( char *repo_name)
     int status = PDS_FILE_ERROR;
     FILE *fp_data;
     FILE *fp_index;
+
     char *data_file = malloc(strlen(repo_name)+strlen(".dat"));
     char *index_file = malloc(strlen(repo_name)+strlen(".ndx"));
     strcpy(data_file,repo_name);
@@ -23,12 +24,14 @@ int pds_create( char *repo_name)
 
     fp_data = fopen(data_file,"ab+");
     fp_index = fopen(index_file,"ab+");
+    
     if(!fp_data || !fp_index){
         printf("FILE OPENING ERROR\n");
         return status;
     }
     fclose(fp_data);
     fclose(fp_index);
+
     status = PDS_SUCCESS;
     return status;
 }
@@ -47,28 +50,39 @@ int pds_open( char *repo_name , int rec_size)
 
     fp_data = fopen(data_file,"ab+");
     fp_index = fopen(index_file,"ab+");
+
     if(!fp_data || !fp_index){
         printf("FILE OPENING ERROR\n");
         return status;
     }
+
     strcpy(repo_handle.pds_name,repo_name);
     repo_handle.pds_data_fp = fp_data;
-    repo_handle.pds_data_fp= fp_index;
+    repo_handle.pds_ndx_fp= fp_index;
     repo_handle.rec_size = rec_size;
-    status = PDS_REPO_OPEN;
-    return PDS_SUCCESS;
+    repo_handle.repo_status = PDS_REPO_OPEN;
+
+    status = PDS_SUCCESS;
+    return status;
 }
 
 int put_rec_by_key( int key ,void *rec )
 {
+    int status = PDS_ADD_FAILED;
+    if(repo_handle.repo_status != PDS_REPO_OPEN)
+        return status;
+
     struct PDS_NdxInfo *index = (struct PDS_NdxInfo *)malloc(sizeof(struct PDS_NdxInfo));
-    fseek(repo_handle.pds_data_fp, 0 ,SEEK_END);
     long int offset = ftell(repo_handle.pds_data_fp);
+
+    fseek(repo_handle.pds_data_fp, 0 ,SEEK_END);
     fwrite(rec,repo_handle.rec_size,1,repo_handle.pds_data_fp);
     index->key = key;
     index->offset = offset;
     bst_add_node(&repo_handle.pds_bst, key, index);
-    return PDS_SUCCESS;
+
+    status = PDS_SUCCESS;
+    return status;
 }
 
 int get_rec_by_key( int key ,void *rec )
@@ -81,16 +95,30 @@ int get_rec_by_key( int key ,void *rec )
     struct PDS_NdxInfo *index = (struct PDS_NdxInfo*)malloc(sizeof(struct PDS_NdxInfo));
     index = node->data;    
     int offset = index->offset;
+
     fseek(repo_handle.pds_data_fp, offset, SEEK_SET);
     fread(rec, repo_handle.rec_size, 1, repo_handle.pds_data_fp);
-    return PDS_SUCCESS;
-
     
+    status = PDS_SUCCESS;
+    return status;
 }
 
 int pds_close()
-{
-    fclose(repo_handle.pds_data_fp);
-    return PDS_SUCCESS;
+{   
+    int status = PDS_FILE_ERROR;
+    FILE *fp_data = repo_handle.pds_data_fp;
+    FILE *fp_index = repo_handle.pds_ndx_fp;
+    
+    if(repo_handle.repo_status != PDS_REPO_OPEN)
+        return status;
+
+    if(!fp_index)
+        printf("HAAHAHAHAH\n");
+    repo_handle.repo_status = PDS_REPO_CLOSED;
+    fclose(fp_data);
+    fclose(fp_index);
+
+    status = PDS_SUCCESS;
+    return status;
 }
 
